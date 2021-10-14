e2c8b5cc84dd7c0b103479ee6ba8b6582c459e95
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-10-14 15:51:47 -0500

    Work in progress

--- 1_create_clusters.sh
+++ 1_create_clusters.sh
@@ -1,9 +1,11 @@
 . /srv/login.sh
 oc-login 1
 for id in $(seq -w 01 1); do
-  yaml=odf-cluster.user$id.yaml
-  config=$(perl -pe "s/adminlab/user$id/g" install-config.yaml.tmpl | base64 -w0)
-  perl -pe "s/adminlab/user$id/g" odf-cluster.yaml.tmpl |
+  name=user$id
+  yaml=odf-cluster.$name.yaml
+  config=$(perl -pe "s/adminlab/$name/g" install-config.yaml.tmpl | base64 -w0)
+  perl -pe "s/adminlab/$name/g" odf-cluster.yaml.tmpl |
     perl -pe "s/(\s+install-config.yaml\:\s*).*$/\$1$config/" > $yaml
   oc apply -f $yaml
+  oc label ManagedCluster --overwrite=true -l name=$name workshop=ocp-odf-admin
 done
