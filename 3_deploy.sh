e6da4f56394658ba1240150a6e10eebc1e05cdc4
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-10-14 21:24:53 -0500

    Automate configuration

--- 3_deploy.sh
+++ 3_deploy.sh
@@ -6,5 +6,7 @@ deployments=''
 for cluster in $(oc get ManagedCluster -o name | cut -d\/ -f2 | sort -n | egrep "^$prefix"); do
   secret=$(oc get secret -n $cluster -l hive.openshift.io/secret-type=kubeadmincreds -o json | jq -r '.items[0].data.password' | base64 -d)
   base_url="$cluster.$(oc get ClusterDeployment -n $cluster -o=jsonpath='{.items[0].spec.baseDomain}')"
-  su - -c "$(printf '%s/deployment/setup.sh %s %s %s\n' "$home" "$cluster" "$base_url" "$secret")" > $cluster.log &
+  perl -pe "s/\{BASE_URL\}/$base_url/g" dashboard-secrets.yaml.tmpl |
+    perl -pe "s/\{CLUSTER_NAME\}/$cluster/g" |
+    perl -pe "s/\{KUBEADMIN_PASSWORD\}/$secret/g" | oc apply -f -
 done
