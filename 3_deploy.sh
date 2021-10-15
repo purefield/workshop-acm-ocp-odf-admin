1ef65221e02a38bd800e8ba0abd0e9cc04cf9b6e
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-10-14 21:34:04 -0500

    Automate configuration

--- 3_deploy.sh
+++ 3_deploy.sh
@@ -2,7 +2,9 @@ prefix=user
 home=$( cd "$(dirname "$BASH_SOURCE")" ; pwd -P )
 . /srv/login.sh
 oc-login 1
-deployments=''
+cd multi-cloud
+oc apply -f namespace.yaml -f secrets.yaml -f application.yaml -f channel.yaml -f subscription.yaml -f placementrule.yaml
+cd -
 for cluster in $(oc get ManagedCluster -o name | cut -d\/ -f2 | sort -n | egrep "^$prefix"); do
   secret=$(oc get secret -n $cluster -l hive.openshift.io/secret-type=kubeadmincreds -o json | jq -r '.items[0].data.password' | base64 -d)
   base_url="$cluster.$(oc get ClusterDeployment -n $cluster -o=jsonpath='{.items[0].spec.baseDomain}')"
