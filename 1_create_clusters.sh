95dee626beb516068e5eb3b6bac7d0bd5ea53ef8
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-10-14 20:39:34 -0500

    Removing sensitive information

--- 1_create_clusters.sh
+++ 1_create_clusters.sh
@@ -1,11 +1,21 @@
 . /srv/login.sh
 oc-login 1
+rm workshop_id_rsa* -f
+ssh-keygen -qP '' -C 'workshop-root' -f workshop_id_rsa
+export $(cat secrets.env)
 for id in $(seq -w 01 2); do
   name=user$id
   yaml=odf-cluster.$name.yaml
-  config=$(perl -pe "s/adminlab/$name/g" install-config.yaml.tmpl | base64 -w0)
+  config=$(perl -pe "s/adminlab/$name/g" install-config.yaml.tmpl | 
+    perl -pe 's/SSH_PUB_KEY/`cat workshop_id_rsa.pub | perl -pe "s|\n||g"`/e' |
+    base64 -w0)
   perl -pe "s/adminlab/$name/g" odf-cluster.yaml.tmpl |
+    perl -pe 's/AWS_ACCESS_KEY_ID/`echo -n \$AWS_ACCESS_KEY_ID`/e' |
+    perl -pe 's/AWS_SECRET_ACCESS_KEY/`echo -n \$AWS_SECRET_ACCESS_KEY`/e' |
+    perl -pe 's/BASE_DOMAIN/`echo -n \$BASE_DOMAIN`/e' |
+    perl -pe 's/SSH_PRIVATE_KEY.$/`cat workshop_id_rsa | perl -pe "s|^|    |g"`/se' |
+    perl -pe 's/DOCKER_CONFIG_JSON.$/`cat dockerconfig.json`/se' |
     perl -pe "s/(\s+install-config.yaml\:\s*).*$/\$1$config/" > $yaml
-  oc apply -f $yaml
-  oc label ManagedCluster --overwrite=true -l name=$name workshop=ocp-odf-admin
+    oc apply -f $yaml
+    oc label ManagedCluster --overwrite=true -l name=$name workshop=ocp-odf-admin
 done
